6f90f255df15751cf5565f52917fe85ea0207501
Carl <carltracy@gmail.com>
2013-04-24 13:15:55 -0400

    test rsvp

--- script/ui.js
+++ script/ui.js
@@ -9,10 +9,11 @@
     
     $("#rsvpForm,#bgOverlay").fadeIn(400,
         function() {
-            $("#rsvpForm").animate({height:"3px",width:"3px",opacity:1,margin:"50% auto",top:"-3px"},400,
+            $("#rsvpForm").top(hTot/2.0 - 1.5);
+            $("#rsvpForm").animate({height:"3px",width:"3px",opacity:1},400,
                 function() {
                     $("#rsvpForm").animate({width:"50%",left:"25%",borderWidth:"1px"},400,
-                        function() { $("#rsvpForm").animate({height:0.5*hTot,borderWidth:"3px",margin:"25% auto"},400,
+                        function() { $("#rsvpForm").animate({height:0.5*hTot,top:0.25*hTot,borderWidth:"3px",margin:"25% auto"},400,
                             function(){ $("#rsvpContent").fadeIn(200) }
                         )}
                     );
