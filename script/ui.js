f9236c56fc29c64340e7de03357720f8818eeeae
Carl <carltracy@gmail.com>
2013-04-23 20:24:46 -0400

    test rsvp

--- script/ui.js
+++ script/ui.js
@@ -10,24 +10,31 @@
             $("#rsvp").click( function() {
                 //alert("CLICK!");
                 $("body").append('<div class="modalOverlay" id="bgOverlay"></div>');
-                $("#bgOverlay #rsvpOverlay").click( function() {
-                    $("#rsvpOverlay").animate({height:"1%",left:0}, 2000,
+                
+                $("#bgOverlay").fadeIn(200, function() {
+                    $("#bgOverlay, #rsvpOverlay").click( function() {
+                        $("#rsvpOverlay").animate({height:"1%",left:0}, 2000,
+                            function() {
+                                $("#rsvpOverlay").animate({width:"0%", opacity:0, top:0}, 500,
+                                    function() { $("#rsvpOverlay").hide(
+                                        function() {
+                                            $("#bgOverlay").fadeOut(500).remove();    
+                                        }    
+                                    ); 
+                                });
+                            }
+                        );
+                        
+                        
+                        
+                    });
+                    
+                    $("#rsvpOverlay").show().css("height","2px").animate({width:"100%",left:0}, 1000,
                         function() {
-                            $("#rsvpOverlay").animate({width:"0%", opacity:0, top:0}, 500,
-                                function() { $("#rsvpOverlay").hide(); }
-                            );
+                            $("#rsvpOverlay").animate({height:"100%", opacity:1, top:0}, 500);
                         }
                     );
-                    
-                    $("#bgOverlay").fadeOut(500).remove();
-                    
                 });
-                
-                $("#rsvpOverlay").show().css("height","2px").animate({width:"100%",left:0}, 1000,
-                    function() {
-                        $("#rsvpOverlay").animate({height:"100%", opacity:1, top:0}, 500);
-                    }
-                );
             
                     
                 
