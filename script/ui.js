344d98ced5d569ea9407ba900fe5eab184dfb9c7
Carl <carltracy@gmail.com>
2013-04-23 20:19:27 -0400

    test rsvp

--- script/ui.js
+++ script/ui.js
@@ -10,10 +10,27 @@
             $("#rsvp").click( function() {
                 //alert("CLICK!");
                 $("body").append('<div class="modalOverlay" id="bgOverlay"></div>');
+                $("#bgOverlay #rsvpOverlay").click( function() {
+                    $("#rsvpOverlay").animate({height:"1%",left:0}, 2000,
+                        function() {
+                            $("#rsvpOverlay").animate({width:"0%", opacity:0, top:0}, 500,
+                                function() { $("#rsvpOverlay").hide(); }
+                            );
+                        }
+                    );
+                    
+                    $("#bgOverlay").fadeOut(500).remove();
+                    
+                });
                 
-                $("#bgOverlay").click( function() {
-                    $("#bgOverlay").remove();
-                })
+                $("#rsvpOverlay").show().css("height","2px").animate({width:"100%",left:0}, 1000,
+                    function() {
+                        $("#rsvpOverlay").animate({height:"100%", opacity:1, top:0}, 500);
+                    }
+                );
+            
+                    
+                
             });
         } 
     );
