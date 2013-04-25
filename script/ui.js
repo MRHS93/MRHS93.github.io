9504a63027f9379fc58edae54a14c6e62aaab70b
Carl <carltracy@gmail.com>
2013-04-25 11:19:32 -0400

    custom easing for in/out animations

--- script/ui.js
+++ script/ui.js
@@ -1,8 +1,40 @@
- 
- function min(a,b) {
+
+var SCL = 250;
+
+function min(a,b) {
     if (a > b) return b;
     else return a;
  }
+ 
+function sinh(aValue) {
+    var myTerm1 = Math.pow(Math.E, aValue);
+    var myTerm2 = Math.pow(Math.E, -aValue);
+    return (myTerm1-myTerm2)/2;
+}
+
+function cosh(aValue) {
+    var myTerm1 = Math.pow(Math.E, aValue);
+    var myTerm2 = Math.pow(Math.E, -aValue);
+    return (myTerm1+myTerm2)/2;
+}
+
+function tanh(aValue) {
+    return sinh(aValue) / cosh(aValue);
+}
+
+
+var easeSclT = 3;
+var easeScl  = 1.0 / sinh(easeSclT);
+
+$.easing.speedIn = function(t, millisecondsSince, startValue, endValue, totalDuration) {
+    //return (sinh((x - 0.5) * 5) + sinh(-(x - 0.5)) + (sinh(2.5) + Math.sin(-2.5))) /    (sinh(2.5) * 1.82);
+    return easeScl * sinh(easeSclT * t);
+};
+
+$.easing.speedOut = function(t, millisecondsSince, startValue, endValue, totalDuration) {
+    //return (sinh((x - 0.5) * 5) + sinh(-(x - 0.5)) + (sinh(2.5) + Math.sin(-2.5))) /    (sinh(2.5) * 1.82);
+    return tanh(easeSclT * t);
+};
  
  function rsvpIn() {
     $("body").append('<div class="modalOverlay" id="bgOverlay"></div>');
@@ -15,13 +47,13 @@
     
     $("#rsvpForm").css("top",hTot/2.0 - 1.5+"px").css("left",wTot/2.0 - 1.5+"px");
 
-    $("#rsvpForm,#bgOverlay").fadeIn(200,
+    $("#rsvpForm,#bgOverlay").fadeIn(2 * SCL, 'speedIn',
         function() {
-            $("#rsvpForm").animate({height:"3px",width:"3px"},100,
+            $("#rsvpForm").animate({height:"3px",width:"3px"},SCL,  'speedIn',
                 function() {
-                    $("#rsvpForm").animate({width:w+"px",left:padW+"px",borderWidth:"1px"},150,
-                        function() { $("#rsvpForm").animate({height:h+"px",top:padT+"px",borderWidth:"3px"},150,
-                            function(){ $("#rsvpContent").fadeIn(200) , fillForm( ); }
+                    $("#rsvpForm").animate({width:w+"px",left:padW+"px",borderWidth:"1px"},2 * SCL,  'speedIn',
+                        function() { $("#rsvpForm").animate({height:h+"px",top:padT+"px",borderWidth:"3px"},SCL,  'speedIn',
+                            function(){ $("#rsvpContent").fadeIn(SCL,  'speedIn') , fillForm( ); }
                         )}
                     );
                 }
@@ -42,10 +74,10 @@ function rsvpOut() {
     
     $("#rsvpContent").fadeOut(200,
         function() {
-            $("#rsvpForm").animate({height:"3px",top:(hTot/2.0 - 1.5) +"px",borderWidth:"0"},300,
+            $("#rsvpForm").animate({height:"3px",top:(hTot/2.0 - 1.5) +"px",borderWidth:"0"}, SCL ,'speedOut',
                 function() {
-                    $("#rsvpForm").animate({width:"3px",left:(wTot/2.0-1.5)+"px"},300,
-                        function() { $("#bgOverlay,#rsvpForm").fadeOut(200 , function(){$("#bgOverlay").remove();}) }
+                    $("#rsvpForm").animate({width:"3px",left:(wTot/2.0-1.5)+"px"}, 2 * SCL,'speedOut',
+                        function() { $("#bgOverlay,#rsvpForm").fadeOut(200 ,'speedOut' , function(){$("#bgOverlay").remove();}) }
                     );
                 }
             );
