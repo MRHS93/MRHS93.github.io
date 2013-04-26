e55ebc1bd5fd5e3c673396a81e732e505a34a069
Carl <carltracy@gmail.com>
2013-04-25 23:34:19 -0400

    moved google web font script to separate js file

---
+++ script/gFonts.js
@@ -0,0 +1,13 @@
+/* 'Great+Vibes::latin', 'Headland+One::latin', 'Ovo::latin', 'Copse::latin', 'Tangerine:700:latin', 'Squada+One::latin', 'Arizonia::latin', 'Cutive::latin', 'Fjalla+One::latin'  */
+var WebFontConfig = {                
+        google: { families: [  'Great+Vibes::latin', 'Headland+One::latin', 'Cutive::latin' ] }
+    };
+    (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})(); 
