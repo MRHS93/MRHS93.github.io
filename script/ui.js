cb460133c5eec62a2e76bed28891b0b5909c5331
Carl <carltracy@gmail.com>
2013-05-09 09:49:25 -0400

    updated submit

--- script/ui.js
+++ script/ui.js
@@ -45,6 +45,11 @@ function tanh(aValue) {
     return sinh(aValue) / cosh(aValue);
 }
 
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+}
+
 /*
     EM / PX from https://raw.github.com/filamentgroup/jQuery-Pixel-Em-Converter/master/pxem.jQuery.js
 */
@@ -56,7 +61,7 @@ function tanh(aValue) {
  * Dual licensed under the MIT (filamentgroup.com/examples/mit-license.txt) or GPL (filamentgroup.com/examples/gpl-license.txt) licenses.
  * Article: http://www.filamentgroup.com/lab/update_jquery_plugin_for_retaining_scalable_interfaces_with_pixel_to_em_con/
  * Options:
- scope: string or jQuery selector for font-size scoping	
+ scope: string or jQuery selector for font-size scoping    
  * Usage Example: $(myPixelValue).toEm(); or $(myEmValue).toPx();
 --------------------------------------------------------------------*/
 
@@ -174,10 +179,8 @@ if (jQuery) {
             setupEventHandlers();
             var frm = $("#rsvpFormData");
             if (frm) {
-                //fillForm(frm);
                 readGetData();
                 minHeight = $(21).toPx() ;
-                //alert(minHeight);
             }
         } 
     );
@@ -198,11 +201,21 @@ function setupEventHandlers(){
     $("#rsvpFormData").submit(
         function(e) {
             e.preventDefault();
+
+            var email = getFormVal("email");
+            var valid = isValidEmailAddress(email);
             
-            var frm = $("#rsvpFormData");
-            if (frm) {
-                //saveRSVPcookie();
-            }            
+            if (valid){
+                var name = getFormVal("name");
+                var r1 = getFormVal("reply1");
+                var r2 = getFormVal("reply2");
+                setFormDataAjax(name,email,r1,r2);
+            }
+            
+            // var frm = $("#rsvpFormData");
+            // if (frm) {
+            //     //saveRSVPcookie();
+            // }            
             
             return false;
         }
@@ -217,7 +230,6 @@ function setupEventHandlers(){
     */
     
 	$('#name').blur(
-		
 		function(event,ui) {
 			if (!($('#key').val())) {
 				var id = people.vlookup($('#name').val(),1,true);
@@ -427,18 +439,9 @@ var people = [
         $( "#name" ).autocomplete({
           source: names ,
           close: 
-          	function(event,ui) {
-          		console.log($('#name').val());
-          		var id = people.vlookup($('#name').val(),1,true);
-          		//alert(id);
-          		if (id) {
-          			$('#key').val(id);
-          			getFormDataAjax();
-          		} else {
-          			$('#key').val( Math.floor(Math.random()*10000000)  );
-          		}
-          	}
-          	
+            function(event,ui) {
+                getKey();
+            }
           
         }); 
     })
@@ -448,6 +451,18 @@ var people = [
 /*
     Data
 */
+
+function getKey() {
+    console.log($('#name').val());
+    var id = people.vlookup($('#name').val(),1,true);
+    //alert(id);
+    if (id) {
+    $('#key').val(id);
+    getFormDataAjax();
+    } else {
+    $('#key').val( Math.floor(Math.random()*10000000)  );
+    }    
+}
  
 function setFormValueWithVal(pName,pVal) {
      if (pVal) {
@@ -510,4 +525,24 @@ function getFormDataAjax(){
         );   
     //}
  }
-     
+
+function setFormDataAjax(name,email,r1,r2){
+    var key = getFormVal("key");
+    if (!key) {
+        getKey();
+        key = getFormVal("key");
+    }
+    $.getJSON('http://theycallmecarl.com/mrhs93/rsvp.php?callback=?','intent=1&key=' + key + 'name=' + name +  '&email=' + email +  '&reply1=' + r1 +  '&reply2=' + r2,
+        function(res) {
+            //console.log("got reply");
+            //console.log(res);
+            var status = res.status;
+            
+            if (status === "ok") {
+                console.log("updated");
+            }
+            
+
+        }
+    );   
+ }
